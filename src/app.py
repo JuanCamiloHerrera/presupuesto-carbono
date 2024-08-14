10960ed9b145674718df1711790df83c93603beb
JuanCamiloHerrera <84823655+JuanCamiloHerrera@users.noreply.github.com>
2024-08-14 05:31:19 -0500

    New app.py file

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Feb 23 12:40:17 2024
 
-@author: camilo
+@author: camilo herrera
 
 Dashboard of net GHG emissions pathways and its correspondend quinquennial budget
 """
@@ -14,25 +14,30 @@ from dash import html
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
+import os    
 
 ############## Brownian motion random walk ###################
 S = 2020 # starting year
-V0 = np.mean([218,253]) # starting value by 2022 as the average between confidence interval
+#V0 = np.mean([218,253]) # starting value by 2022 as the average between confidence interval
+V0 = 280 # starting value for 2021 from the inventory
 d = 500000 # number of runs
-T = 15 # years ahead
+T = 50 # years ahead
 n = T + 1 # number of observations
 times = np.linspace(S, S+T, n)
-dB = 30 * np.random.normal(size=(n - 1 - 2, d)) # random walk # 30 is heuristic as an annual absolute yearly growth # -2 is for the three initial years but the first one is removed
-B1 = np.transpose(np.random.uniform(np.array([236, 247, 218]),np.array([270, 282, 253]), size=(d, 3))) # random values for 2020, 2021 and 2022 based on confidence interval of forecasted values
-B0 = np.full(shape=(1, d), fill_value=V0) # initial value for 2022
+actual_values = np.repeat(np.array([275, 280])[:, np.newaxis], d, axis=1) # Actual GHG inventory values for 2020 and 2021
+V2 = np.mean([260, 300]) # Starting value for 2022
+#dB = 40 * np.random.normal(size=(n - 1 - 1, d)) # random walk # 30 is heuristic as an annual absolute yearly growth # -1 is for the two initial years but the first one is removed
+dB = 30 * np.random.uniform(low=-1, high=1, size=(n - 1 - 1, d)) #using a uniform distribution instead of normal
+#B1 = np.transpose(np.random.uniform(np.array([236, 247, 218]),np.array([270, 282, 253]), size=(d, 3))) # random values for 2020, 2021 and 2022 based on confidence interval of forecasted values
+B0 = np.full(shape=(1, d), fill_value=V2) # initial value for 2022
 B = np.cumsum(np.concatenate((B0,dB), axis=0),axis=0) # accumulated sum of random walk with initia value B0
-B = np.concatenate((B1,B[1:,]), axis=0) #B1 is concatenated to B where its first year is removed
+B = np.concatenate((actual_values,B[1:,]), axis=0) #Actual values are concatenated to B where its first year is removed
 
 budgets=np.array([np.sum(B[0:6],axis=0),np.sum(B[6:11],axis=0),np.sum(B[11:16],axis=0),np.sum(B[16:21],axis=0),np.sum(B[21:26],axis=0),np.sum(B[26:31],axis=0)])
 
 # Define initial values for budget0 and budget1
-budget0 = 1550
-budget1 = 1000
+budget0 = 1650
+budget1 = 1050
 # Defining boolean inequalities based on budget initial values
 array1 = budgets[0] <= budget0
 array2 = budgets[0] >= budget0 - 10
@@ -100,7 +105,7 @@ app.layout = html.Div([
                 html.Div([
                     html.Div([
                         html.Label('2025:'),
-                        dcc.Input(id='value-2025', type='number', min=0, max=400, value=248),
+                        dcc.Input(id='value-2025', type='number', min=0, max=400, value=270),
                     ], style={'display': 'flex', 'align-items': 'center'}),
                 
                     html.Div(style={'width': '20px'}),
@@ -114,21 +119,21 @@ app.layout = html.Div([
                 
                     html.Div([
                         html.Label('2035:'),
-                        dcc.Input(id='value-2035', type='number', min=0, max=400, value=120),
+                        dcc.Input(id='value-2035', type='number', min=0, max=400, value=140),
                     ], style={'display': 'flex', 'align-items': 'center'}),
                 
                     html.Div(style={'width': '20px'}),
                 
                     html.Div([
                         html.Label('2040:'),
-                        dcc.Input(id='value-2040', type='number', min=0, max=400, value=75),
+                        dcc.Input(id='value-2040', type='number', min=0, max=400, value=110),
                     ], style={'display': 'flex', 'align-items': 'center'}),
                 
                     html.Div(style={'width': '20px'}),
                 
                     html.Div([
                         html.Label('2045:'),
-                        dcc.Input(id='value-2045', type='number', min=0, max=400, value=35),
+                        dcc.Input(id='value-2045', type='number', min=0, max=400, value=75),
                     ], style={'display': 'flex', 'align-items': 'center'}),
                 ], style={'display': 'flex', 'flex-wrap': 'wrap'})
 
@@ -207,23 +212,23 @@ app.css.append_css({
 def update_chart(value_2025, value_2030, value_2035, value_2040, value_2045):
     
     # Scenarios
-    year = [2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030]
-    lower = [293, 292, 270, 249, 229, 209, 189, 169]
-    upper = [318, 319, 316, 294, 266, 237, 209, 180]
-    ndc = [262,257,250,246,245,236,229,218]
+    year = [2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030]
+    lower = [294, 293, 292, 270, 249, 229, 209, 189, 169]
+    upper = [306, 318, 319, 316, 294, 266, 237, 209, 180]
+    ndc = [266,262,257,250,246,245,236,229,218]
     
-    year1 = [2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 2047, 2048, 2049, 2050]
-    lower1 = [213, 199, 187, 177, 167, 157, 147, 135, 128, 120, 113, 105, 98, 92, 85, 79, 72, 65, 58, 52, 46, 39, 33, 26, 19, 13, 5, 0]
-    upper1 = [237, 226, 217, 210, 205, 200, 194, 189, 180, 172, 163, 154, 146, 139, 133, 126, 120, 114, 106, 99, 93, 87, 80, 73, 67, 60, 53, 48]
+    year1 = [2022,2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 2047, 2048, 2049, 2050]
+    lower1 = [227, 213, 199, 187, 177, 167, 157, 147, 135, 128, 120, 113, 105, 98, 92, 85, 79, 72, 65, 58, 52, 46, 39, 33, 26, 19, 13, 5, 0]
+    upper1 = [245, 237, 226, 217, 210, 205, 200, 194, 189, 180, 172, 163, 154, 146, 139, 133, 126, 120, 114, 106, 99, 93, 87, 80, 73, 67, 60, 53, 48]
     
-    year2 = [2020, 2021, 2022]
-    mean2 = [253, 264, 236]
-    lower2 = [236, 247, 218]
-    upper2 = [270, 282, 253]
+    year2 = [2020, 2021]
+    mean2 = [275, 280]
+    #lower2 = [236, 247, 218]
+    #upper2 = [270, 282, 253]
     
-    year3 = [2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 2047, 2048, 2049, 2050]
-    ccdr_wb = [264, 250, 239, 227, 212, 200, 187, 174, 161, 149, 140, 128, 119, 111, 100, 89, 79, 68, 58, 49, 44, 36, 30, 23, 17, 10, 4, 0]
-    bid = [206, 200, 193, 181, 168, 155, 143, 130, 126, 122, 118, 114, 110, 105, 100, 95, 89, 84, 77, 69, 62, 54, 46, 39, 32, 25, 17, 10]
+    year3 = [2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 2047, 2048, 2049, 2050]
+    ccdr_wb = [273, 264, 250, 239, 227, 212, 200, 187, 174, 161, 149, 140, 128, 119, 111, 100, 89, 79, 68, 58, 49, 44, 36, 30, 23, 17, 10, 4, 0]
+    bid = [213, 206, 200, 193, 181, 168, 155, 143, 130, 126, 122, 118, 114, 110, 105, 100, 95, 89, 84, 77, 69, 62, 54, 46, 39, 32, 25, 17, 10]
 
     
     # Set color for all traces
@@ -243,9 +248,9 @@ def update_chart(value_2025, value_2030, value_2035, value_2040, value_2045):
     trace_upper1 = go.Scatter(x=year1, y=upper1, mode='lines', name='E2050', fill='tonexty', fillcolor=e2050_color, line=dict(color=e2050_color))
 
     # INGEI
-    trace_lower2 = go.Scatter(x=year2, y=lower2, mode='lines', name='Intervalo de confianza estimación INGEI', line=dict(color=inventario_color), showlegend=False)
-    trace_upper2 = go.Scatter(x=year2, y=upper2, mode='lines', name='Intervalo de confianza estimación INGEI', fill='tonexty', fillcolor=inventario_color, line=dict(color=inventario_color), showlegend=False)
-    trace_mean2 = go.Scatter(x=year2, y=mean2, mode='markers+lines', name='Estimación INGEI', line=dict(color='rgba(233, 113, 50, 0.6)'))
+    #trace_lower2 = go.Scatter(x=year2, y=lower2, mode='lines', name='Intervalo de confianza estimación INGEI', line=dict(color=inventario_color), showlegend=False)
+    #trace_upper2 = go.Scatter(x=year2, y=upper2, mode='lines', name='Intervalo de confianza estimación INGEI', fill='tonexty', fillcolor=inventario_color, line=dict(color=inventario_color), showlegend=False)
+    trace_mean2 = go.Scatter(x=year2, y=mean2, mode='markers+lines', name='INGEI', line=dict(color='rgba(233, 113, 50, 0.6)'))
 
     # Single scatter
     trace_ccdr_wb = go.Scatter(x=year3, y=ccdr_wb, mode='markers+lines', name='CCDR-WB', marker=dict(color=ccdr_color, size=6, symbol='x'))
@@ -256,13 +261,14 @@ def update_chart(value_2025, value_2030, value_2035, value_2040, value_2045):
     updated_chart_layout = go.Layout(title='Escenarios de emisiones netas GEI Colombia', yaxis=dict(title='MtCO2eq'), xaxis=dict(title='Año'),margin=dict(t=150))
 
     # Create figure for the updated chart
-    updated_chart_fig = go.Figure(data=[trace_lower, trace_upper, trace_lower1, trace_upper1, trace_lower2, trace_upper2, trace_mean2, trace_ccdr_wb, trace_bid, trace_ndc], layout=updated_chart_layout)
+    #updated_chart_fig = go.Figure(data=[trace_lower, trace_upper, trace_lower1, trace_upper1, trace_lower2, trace_upper2, trace_mean2, trace_ccdr_wb, trace_bid, trace_ndc], layout=updated_chart_layout)
+    updated_chart_fig = go.Figure(data=[trace_lower, trace_upper, trace_lower1, trace_upper1, trace_mean2, trace_ccdr_wb, trace_bid, trace_ndc], layout=updated_chart_layout)
 
     # Static datapoints at the beginning and end
-    updated_chart_fig.add_trace(go.Scatter(x=[2020, 2050], y=[253, 0], mode='markers', marker=dict(size=15, color='blue'), name=None, showlegend=False))
+    updated_chart_fig.add_trace(go.Scatter(x=[2021, 2050], y=[280, 0], mode='markers', marker=dict(size=15, color='blue'), name=None, showlegend=False))
 
     # Connecting lines
-    updated_chart_fig.add_trace(go.Scatter(x=[2020, 2025, 2030, 2035, 2040, 2045, 2050], y=[253, value_2025, value_2030, value_2035, value_2040, value_2045, 0], mode='lines', line=dict(color='blue',width=4), name='Senda emisiones netas'))
+    updated_chart_fig.add_trace(go.Scatter(x=[2021, 2025, 2030, 2035, 2040, 2045, 2050], y=[280, value_2025, value_2030, value_2035, value_2040, value_2045, 0], mode='lines', line=dict(color='blue',width=4), name='Senda emisiones netas'))
 
     # Data points as markers
     updated_chart_fig.add_trace(go.Scatter(x=[2025, 2030, 2035, 2040, 2045], y=[value_2025, value_2030, value_2035, value_2040, value_2045], mode='markers', marker=dict(size=15, color='red'), name='Geosmina', showlegend=False))
@@ -302,11 +308,13 @@ def update_chart1(value_2025, value_2030, value_2035, value_2040, value_2045):
     global df_budget, total_budget
     # Initial static values
     years = list(range(2020, 2051))
-    emissions = [253] + [0] * (len(years) - 2) + [0]
+    emissions = [275,280] + [0] * (len(years) - 3) + [0]
     
     # Create the initial DataFrame
     df = pd.DataFrame({'Year': years, 'NetEmissions': emissions})
     
+    # Convert 'NetEmissions' to float to avoid warnings
+    df['NetEmissions'] = df['NetEmissions'].astype(float)
     
     # Callback years and values (replace these with your actual callback values)
     callback_years = [2025, 2030, 2035, 2040, 2045]
@@ -326,13 +334,21 @@ def update_chart1(value_2025, value_2030, value_2035, value_2040, value_2045):
     # Interpolate values inbetween
     df = df.set_index('Year')
     for i in range(2020,2050,5):
-        for j in range(i+1,i+5):
-            df.loc[j,'NetEmissions'] = df.loc[i,'NetEmissions'] + (df.loc[i+5,'NetEmissions'] - df.loc[i,'NetEmissions'] )*(j-i)/5
-            #df.NetEmissions[df['Year']==j] = df.NetEmissions[df['Year']==i] + (df.NetEmissions[df['Year']==i+5] - df.NetEmissions[df['Year']==i] )*(j-i)/5
+        if i == 2020:
+            for j in range(2022,2025):
+                df.loc[j,'NetEmissions'] = df.loc[2021,'NetEmissions'] + (df.loc[2025,'NetEmissions'] - df.loc[2021,'NetEmissions'] )*(j-2021)/4
+        else:
+            for j in range(i+1,i+5):
+                df.loc[j,'NetEmissions'] = df.loc[i,'NetEmissions'] + (df.loc[i+5,'NetEmissions'] - df.loc[i,'NetEmissions'] )*(j-i)/5
+                #df.NetEmissions[df['Year']==j] = df.NetEmissions[df['Year']==i] + (df.NetEmissions[df['Year']==i+5] - df.NetEmissions[df['Year']==i] )*(j-i)/5
     
+
+
+
     
     #Budget estimation
-    p1=752.914+df.loc[2023:2025,'NetEmissions'].sum() # Mean of estimated emissions from 2020 to 2022 (752.914) plus the output of the pathway form 2023 to 2025
+    #p1=555+df.loc[2022:2025,'NetEmissions'].sum() # net emissions from 2020 to 2021 (555) from the INGEI plus the output of the pathway form 2022 to 2025
+    p1=df.loc[2020:2025,'NetEmissions'].sum()
     p2=df.loc[2026:2030,'NetEmissions'].sum()
     p3=df.loc[2031:2035,'NetEmissions'].sum()
     p4=df.loc[2036:2040,'NetEmissions'].sum()
@@ -353,7 +369,7 @@ def update_chart1(value_2025, value_2030, value_2035, value_2040, value_2045):
         marker_color='steelblue',
         text=round(df_budget['budget']),
         textposition=['inside' if i == 0 else 'auto' for i in range(len(df_budget))],
-        error_y=dict(type='data', array=[52], visible=True, color='magenta', thickness=2, width=4), 
+        #error_y=dict(type='data', array=[52], visible=True, color='magenta', thickness=2, width=4), 
     ))
     
     
@@ -465,6 +481,6 @@ def update_plots(budget0, budget1):
 
 
 
-# app.run_server()  #To run on IDE
+ #app.run_server()  #Para correr en IDE activar esta linea y ocultar la de heroku
 if __name__ == '__main__':
-    app.run_server(debug=True)   #For deployment
+    app.run_server(debug=True)   #Para correr en Heroku app activar esta linea y ocultar la de spyder
